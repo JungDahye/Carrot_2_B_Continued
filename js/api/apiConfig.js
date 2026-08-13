0f97d5fc7e3644c7b2876f6ba6311c46f6d061cf
JungDaHye <dah9316@gmail.com>
2026-08-13 13:41:16 +0900

    fix : api 경로 중복 수정

--- js/api/apiConfig.js
+++ js/api/apiConfig.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://carrot.techfree.kr/api";
+export const API_BASE_URL = "https://carrot.techfree.kr";
 export const API_AUTH_URL = `${API_BASE_URL}/api/auth`;
 export const API_PRODUCT_URL = `${API_BASE_URL}/api/products`;
 export const API_IMAGE_URL = `${API_BASE_URL}/api/images`;
