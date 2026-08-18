28a7d695207b8a0478b7bd5bee951770dad0a079
JungDaHye <dah9316@gmail.com>
2026-08-18 09:28:34 +0900

    feat: api team key ignore에 추가하여 노출방지.

--- js/api/apiConfig.example.js
+++ js/api/apiConfig.example.js
@@ -1,5 +1,5 @@
-export const API_TEAM_KEY = "carrot_f2e26d4c07468a0b6f22510d316922e4";
-export const API_BASE_URL = "https://carrot.techfree.kr";
+export const API_TEAM_KEY = "";
+export const API_BASE_URL = "";
 export const API_AUTH_URL = `${API_BASE_URL}/api/auth`;
 export const API_PRODUCT_URL = `${API_BASE_URL}/api/products`;
 export const API_IMAGE_URL = `${API_BASE_URL}/api/images`;
